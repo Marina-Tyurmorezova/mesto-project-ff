97612841466b3c0638d812cc855cbf7e252d848e
Marina Tyurmorezova <tyurmorezova.m.a@yandex.ru>
2025-03-04 22:29:32 +0300

    начало работы с API

--- src/components/api.js
+++ src/components/api.js
@@ -1,1 +1,33 @@
 //описаны функции для взаимодействия с сервером;
+
+//обращение к серверу по выданному токену
+//return fetch('https://mesto.nomoreparties.co/v1/wff-cohort-31/cards', {
+//    headers: {
+//      authorization: '9c82fa18-41e4-4bdb-920b-b57efab56c1d'
+//    }
+//  })
+//   .then(res => res.json())
+//   .then((result) => {
+//     console.log(result);
+//   });
+
+// Загрузка информации о пользователе с сервера
+//GET https://mesto.nomoreparties.co/v1/:wff-cohort-31/users/me 
+
+fetch('https://mesto.nomoreparties.co/v1/:wff-cohort-31/users/me' , {
+    headers: {
+        authorization: '9c82fa18-41e4-4bdb-920b-b57efab56c1d'
+    }
+    })
+    .then((res) => {
+        if (res.ok) {
+            return res.json();
+        }
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+    })
+    .catch((err) => {
+        console.log(err); 
+    });
+
+
+//Загрузка карточек с сервера GET https://mesto.nomoreparties.co/v1/wff-cohort-31/cards 
